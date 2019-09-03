3c80411d2ac5fc803326286f5432a49182028279
zhuwei287787958 <287787958@qq.com>
2019-09-03 16:41:27 +0800

    no message

--- AppSys/src/main/java/cn/demo/Controller/Dev_UserController.java
+++ AppSys/src/main/java/cn/demo/Controller/Dev_UserController.java
@@ -20,29 +20,46 @@ import cn.demo.entity.User;
  *
  */
 @Controller
-@RequestMapping("/sys/developer")
+@RequestMapping(value="/dev")
 public class Dev_UserController {
 	private Logger log = Logger.getLogger(Dev_UserController.class);
 	@Resource
 	private Dev_UserService dev_UserService;
+	
+	@RequestMapping(value="changeindex")
+	public String changeLogin() {
+		return "index";
+	}
 
 	/**
 	 * 开发者用户登录
 	 */
-	@RequestMapping(value = "/devlogin", method = RequestMethod.GET)
-	public String dev_UserLogin2(
-			@RequestParam(value = "userCode", required = false, defaultValue = "") String userCode,
-			@RequestParam(value = "userPassword", required = false, defaultValue = "") String userPassword,
-			HttpSession session, HttpServletRequest request) {
-		log.info("用户名:" + userCode);
-		User user = dev_UserService.dev_UserLogin(userCode, userPassword);
-		if (null != user) {
+	@RequestMapping(value="/devlogin")
+	public String login(){
+		log.debug("LoginController welcome AppInfoSystem backend==================");
+		return "devlogin";
+	}
+	
+	@RequestMapping(value="/dologin",method=RequestMethod.POST)
+	public String doLogin(@RequestParam String userCode,@RequestParam String userPassword,HttpServletRequest request,HttpSession session){
+		log.debug("doLogin====================================");
+		//调用service方法，进行用户匹配
+		User user = null;
+		try {
+			user = dev_UserService.dev_UserLogin(userCode, userPassword);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(null != user){//登录成功
+			//放入session
 			session.setAttribute("user", user);
-			return "frame";
-		}
-		if (!(userCode.equals("") || userPassword.equals(""))) {
+			//页面跳转（main.jsp）
+			return "redirect:/dev/developer/main";
+		}else{
+			//页面跳转（login.jsp）带出提示信息--转发
 			request.setAttribute("error", "用户名或密码不正确");
+			return "devlogin";
 		}
-		return "developer/devlogin";
 	}
 }
